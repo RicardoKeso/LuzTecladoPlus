694b72bf3634e6ce8fff210830289bd377d205c7
RicardoKeso <ricardo_keso@hotmail.com>
2015-08-12 00:45:41 -0300

    0

--- V01/LuzTeclado.ino
+++ V01/LuzTeclado.ino
@@ -8,14 +8,14 @@ const int ultrassomEcho = 2;
 const int ultrassomPing = 3;
 const int movPower = 5;
 const int movData = 6;
-const int saidaRele = 12;
+const int saidaLed = 12;
 const int sensorLuz = 3; //analogico
 
 void setup(){
   Serial.begin(9600);
   pinMode(movPower, OUTPUT);
   pinMode(ultrassomPing, OUTPUT);
-  pinMode(saidaRele, OUTPUT);
+  pinMode(saidaLed, OUTPUT);
   
   pinMode(movData, INPUT);
   pinMode(ultrassomEcho, INPUT);
@@ -30,9 +30,9 @@ void Luzteclado(){
     if (ssr.SensorMovimento(movPower, movData)){
       ssr.LedPlaca(1, 0);
       int flagDistancia = ssr.SensorDistancia(ultrassomPing, ultrassomEcho, 90, 200);
-      ssr.Rele(saidaRele, flagDistancia);
+      digitalWrite(saidaLed, flagDistancia);
     }
   } else {
-    ssr.Rele(saidaRele, 0);
+    digitalWrite(saidaLed, LOW);
   }
 }
